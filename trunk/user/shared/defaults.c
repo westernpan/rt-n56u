c96e93edc185ec6612eadff2faf845f2c25b572c
westernpan <59242699+westernpan@users.noreply.github.com>
2020-05-31 15:44:39 +0800

    change default dns ip

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -68,8 +68,8 @@ struct nvram_pair router_defaults[] = {
 	{ "wan_ipaddr", "0.0.0.0" },		/* WAN IP address */
 	{ "wan_netmask", "0.0.0.0" },		/* WAN netmask */
 	{ "wan_gateway", "0.0.0.0" },		/* WAN gateway */
-	{ "wan_dnsenable_x", "1" },
-	{ "wan_dns1_x", "" },
+	{ "wan_dnsenable_x", "0" },
+	{ "wan_dns1_x", "223.6.6.6" },
 	{ "wan_dns2_x", "" },
 	{ "wan_dns3_x", "" },
 	{ "wan_hostname", "" },			/* WAN hostname */
